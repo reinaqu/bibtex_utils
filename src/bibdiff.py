154dddf9fb0cf21b6fd702960699681c736727c8
reinaqu <reinaqu@gmail.com>
2022-03-23 16:14:44 +0100

    Added functionality to substract using different fields

--- src/bibdiff.py
+++ src/bibdiff.py
@@ -21,9 +21,17 @@ def main():
     args = parse_arg()
     
     min_database = load_bibtex(args.min[0])
-    sub_databases = load_bibtexs(args.sub)    
+    logging.info(f"Number of entries in the min database bibtex...{num_entries(min_database)}")
+    sub_databases = load_bibtexs(args.sub)   
+    for i in range (len(sub_databases)):
+        logging.info(f"Number of entries in the sub {i+1} database bibtex...{num_entries(sub_databases[i])}" )
+     
+    if args.field:
+        field = args.field
+    else:
+        field = 'ID'
    
-    result = subtract_dbs(min_database, sub_databases)
+    result = subtract_dbs_by_field(min_database, sub_databases, field)
     entries = result.get_entry_list()
     logging.info("Number of entries in the resulting bibtex..."+ str(len(entries)) )
     
@@ -34,11 +42,12 @@ def main():
 
 
 def parse_arg():
-    parser = argparse.ArgumentParser(prog='bibdiff', description='The result is the first database, removing the '
+    parser = argparse.ArgumentParser(prog='bibdiffids', description='The result is the first database, removing the '
                                                                  'references existing in the list of databases'
                                                                  'enumerated in the sub param')
     parser.add_argument('-min', nargs=1, type=str)
     parser.add_argument('-sub', nargs='+', type=str)
+    parser.add_argument('-field', nargs='?')
     parser.add_argument('-o', '--output', nargs='?', help='file')
     return parser.parse_args()
 
--- src/bibutils.py
+++ src/bibutils.py
@@ -12,8 +12,12 @@ import os.path
 import sys
 from collections import Counter
 
-def load_bibtex(filename):
-   
+
+def load_bibtex(filename:str)->bibtexparser.bibdatabase.BibDatabase:
+    '''
+    @param filename: The name (and path) of the bibtex file to be loaded
+    @return: A BibDatabase object with the entries corresponding to the bibtextfile.
+    '''   
     if not (os.path.isfile(filename)):
         print("input file not found"+ filename, file=sys.stderr)
         exit(0)
@@ -22,6 +26,20 @@ def load_bibtex(filename):
     with open(filename,encoding='utf-8') as bibtex_file:
         database = bibtexparser.load(bibtex_file, parser)
     return database 
+
+def num_entries(bibdatabase: bibtexparser.bibdatabase.BibDatabase):
+    return len(bibdatabase.entries)
+
+def load_bibtex_as_text(filename):
+    
+    if not (os.path.isfile(filename)):
+        print("input file not found"+ filename, file=sys.stderr)
+        exit(0)
+        
+    with open(filename,  encoding='utf-8') as bibtex_file:
+        data = bibtex_file.read() #read the whole file in a str
+    return data
+
 
 def load_bibtexs(filenames):
     ''' Get a sequence of filenames and returns a list of databases one for each filename.
@@ -36,6 +54,7 @@ def write_bibtex(db, filename):
     with open(filename, 'w', encoding='utf-8') as bibtex_file:
         writer = BibTexWriter()
         writer.add_trailing_comma = True
+        writer.indent=''
         bibtexparser.dump(db, bibtex_file,writer)
 
 def writes_bibtex(db):
@@ -43,6 +62,13 @@ def writes_bibtex(db):
     Returns a string with the bibtex of the entries contained in the database
     '''
     return bibtexparser.dumps(db)
+
+def write_bibtex_as_text(filename,data):
+    '''
+    Writes the database into the file named filename
+    '''
+    with open(filename, 'w', encoding='utf-8') as bibtex_file:
+        bibtex_file.write(data)
 
 def merge_dbs (db_list):
     res = BibDatabase()
@@ -63,15 +89,72 @@ def subtract_dbs(db_min, list_dbs_sub):
             db.entries.append(entry)
     return db
 
+def subtract_dbs_dois(db_min, list_dbs_sub):
+    """
+    The result of the operation is db_min -  union (db_sub_list).
+    The result is the db_min without the references included in the list_dbs_sub databases 
+    """
+    db_subs = merge_dbs(list_dbs_sub)
+    db = BibDatabase()
+    for entry in db_min.entries:
+        if not contains_doi(entry, db_subs):
+            db.entries.append(entry)
+    return db 
+
+def subtract_dbs_by_field(db_min, list_dbs_sub, field='ID'):
+    """
+    The result of the operation is db_min -  union (db_sub_list).
+    The result is the db_min without the references included in the list_dbs_sub databases 
+    """
+    db_subs = merge_dbs(list_dbs_sub)
+    db = BibDatabase()
+    for entry in db_min.entries:
+        if not contains_field(entry, db_subs, field):
+            db.entries.append(entry)
+    return db 
+
+def subtract_dbs_ids(db_min, list_dbs_sub):
+    """
+    The result of the operation is db_min -  union (db_sub_list).
+    The result is the db_min without the references included in the list_dbs_sub databases 
+    """
+    db_subs = merge_dbs(list_dbs_sub)
+    db = BibDatabase()
+    for entry in db_min.entries:
+        if not contains(entry, db_subs):
+            db.entries.append(entry)
+    return db 
 
 def contains(entry, database):
     """
     check if entry exits in the database, based on its ID
     """
+    res=False
     for ent in database.entries:
         if entry['ID'] == ent['ID']:
-            return True
-    return False
+            res=True
+            break
+    return res
+
+def contains_doi(entry, database):
+    """
+    check if entry exits in the database, based on its doi
+    """
+    return contains_field(entry, database, 'doi')
+
+
+def contains_field(entry, database, field):
+    """
+    check if entry exits in the database, based on its doi
+    """
+    res=False
+    for ent in database.entries:
+        d1= entry.get(field) 
+        d2= ent.get(field)
+        if not d1 == None and not d2 == None and d1==d2:
+            res= True
+            break
+    return res
 
 def count_entries_by_type(entries):
     types = [entry['ENTRYTYPE'] for entry in entries]
@@ -85,10 +168,7 @@ def common_entries (db1, db2):
     ids2 = get_entries_ids(db2.get_entry_list())
     intersect = ids1 & ids2
     return intersect
-
-
-       
-
+	
 def print_database_stats(database):
     entries = database.get_entry_list()
     print ("Number of entries:" , len(entries))
